be832ce85ca41dc8e7c515e601a77c5324cd87f8
salehgomaa60 <salehgomaazzb@gmail.com>
2023-09-20 14:12:55 -0700

    enhancing linked lists

--- shell.h
+++ shell.h
@@ -145,3 +145,8 @@ list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+ssize_t get_node_index(list_t *, list_t *);
+list_t *nodestartswith(list_t *, char *, char);
